2f676eab10f2b55faf9db2327d6430b1fc2c30c9
Mitchell Dowd <mitchell.dowd@live.com>
2017-01-12 15:35:54 +1100

    - Added material themeing for Android API level 21+. - Set include directories for sample app.

--- samples/todo/src/todo.cpp
+++ samples/todo/src/todo.cpp
@@ -1,7 +1,12 @@
-#include "../../../include/native.h"
+// Framework Dependencies
+#include <native.h>
 
+/**
+	The main application creation class.
+ */
 class App : public Native::UI::App
 {
 };
 
+// Entry Point
 NATIVE_UI_APP(App)
