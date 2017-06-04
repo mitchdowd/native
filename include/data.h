0c3c97c6604dd3a967884c518dc10a5277bde52f
Mitchell Dowd <mitchell.dowd@live.com>
2017-06-04 10:36:26 +1000

    Adding IO and Data projects.

---
+++ include/data.h
@@ -0,0 +1,7 @@
+#ifndef _NATIVE_DATA_H_
+#define _NATIVE_DATA_H_ 1
+
+
+
+#endif // _NATIVE_DATA_H_
+
---
+++ include/io.h
@@ -0,0 +1,7 @@
+#ifndef _NATIVE_IO_H_
+#define _NATIVE_IO_H_ 1
+
+
+
+#endif // _NATIVE_IO_H_
+
