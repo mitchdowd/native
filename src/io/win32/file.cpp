6ae724e32c5e70a642b93f2109edbcf1d24f5e29
Mitchell Dowd <mitchell.dowd@live.com>
2017-06-04 20:24:18 +1000

    More Win32 file implementation.

--- src/io/win32/file.cpp
+++ src/io/win32/file.cpp
@@ -31,7 +31,60 @@ namespace native
 
 		void File::open(const String& path, Flags<FileState> state)
 		{
-			throw NotImplementedException();
+			if (_handle)
+				throw IoException("File already open");
+
+			DWORD dwDesiredAccess = 0;
+			DWORD dwCreateDisposition = 0;
+
+			// Check read/write access requested.
+			if (state & FileState::Readable)
+				dwDesiredAccess |= GENERIC_READ;
+			if (state & FileState::Writable)
+				dwDesiredAccess |= GENERIC_WRITE;
+
+			// Check create/open flags.
+			if (state & FileState::Truncate)
+			{
+				if (state & FileState::New)
+					dwCreateDisposition = CREATE_NEW;
+				else if (state & FileState::Existing)
+					dwCreateDisposition = TRUNCATE_EXISTING;
+				else
+					dwCreateDisposition = CREATE_ALWAYS;
+			}
+			else if (state & FileState::Create)
+			{
+				if (state & FileState::New)
+					dwCreateDisposition = CREATE_NEW;
+				else if (state & FileState::Existing)
+					dwCreateDisposition = OPEN_EXISTING;
+				else
+					dwCreateDisposition = OPEN_ALWAYS;
+			}
+			else
+			{
+				if (state & FileState::New)
+					throw IoException("Invalid file state options");
+
+				dwCreateDisposition = OPEN_EXISTING;
+			}
+
+			// Open the file.
+			if ((_handle = ::CreateFile(preparePath(path).toArray(), dwDesiredAccess, FILE_SHARE_READ, NULL, dwCreateDisposition, 0, NULL)) == INVALID_HANDLE_VALUE)
+			{
+				switch (::GetLastError())
+				{
+				case ERROR_FILE_NOT_FOUND:
+					throw IoException("File not found");
+				case ERROR_FILE_EXISTS:
+					throw IoException("File already exists");
+				case ERROR_ACCESS_DENIED:
+					throw IoException("File access denied");
+				default:
+					throw IoException("File open failed");
+				}
+			}
 		}
 
 		void File::close()
@@ -45,12 +98,24 @@ namespace native
 
 		size_t File::read(void* buffer, size_t maxBytes)
 		{
-			throw NotImplementedException();
+			// Convert the size to the required DWORD.
+			DWORD dwSize = maxBytes > MAXDWORD ? MAXDWORD : DWORD(maxBytes);
+
+			if (::ReadFile(_handle, buffer, dwSize, &dwSize, NULL) == FALSE)
+				throw IoException("File read failed");
+
+			return dwSize;
 		}
 
 		size_t File::write(const void* data, size_t bytes)
 		{
-			throw NotImplementedException();
+			// Convert the number of bytes to a DWORD.
+			DWORD dwBytes = bytes > MAXDWORD ? MAXDWORD : DWORD(bytes);
+
+			if (::WriteFile(_handle, data, dwBytes, &dwBytes, NULL) == FALSE)
+				throw IoException("File write failed");
+
+			return dwBytes;
 		}
 
 		void File::deleteFromDisk(const String& path)
